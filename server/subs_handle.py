28b4318c9a8dd814aeccf78c854f1084387c6690
Frank <Frank_Kings@outlook.com>
2023-01-17 15:59:18 -0300

    Se corrigió la función bloqueante por una implementación de los cb de mqtt.

--- server/subs_handle.py
+++ server/subs_handle.py
@@ -31,8 +31,10 @@ def topic_verify(msg):
             print("topic name: ["+topic_name+"]")
             subprocess.run(["bash", "sensor_create.sh", topic_name])
 
-            #mala idea, blokea el loop. buscar otra manera
+            #mala idea, blokea el loop. buscar otra manera de suscribirse
             #subprocess.run(["bash", "./sensors/"+topic_name+"/run.sh"])
+
+            client.subscribe(msg.payload.decode("utf-8"))
 
 
 
@@ -48,13 +50,19 @@ def topic_verify(msg):
 # Callback al recibir un mensaje
 def on_message(client, userdata, msg):
     #print(msg.topic + " " + str(msg.payload))
-
+    print("message received")
     
     register_topic = "test/topics"
 
     if msg.topic == register_topic:
+                    print("topic: register")
                     topic_verify(msg)
 
+    else:
+                    print("topic: sensor")
+                    #registrar en la base de datos
+                    with open("./sensors/"+msg.topic+"/measures.txt", "a") as file:
+                                    file.write(msg.payload.decode("utf-8")+"\n")
 
 
 
